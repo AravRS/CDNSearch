4474eec5790c87e026e223228a66048cd60318d8
Aravindh RS <aravindh.ravishankar@gmail.com>
2020-08-22 12:44:03 +0530

    Added click-to-copy and tooltip

--- src/Lib.js
+++ src/Lib.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import ReactTooltip from "react-tooltip";
 import "./Libs.css";
 
 export default function Lib({ lib }) {
@@ -6,12 +8,29 @@ export default function Lib({ lib }) {
 
   return (
     <div className="libs-container">
-      <h1 className="libs-name">{lib.name}</h1>
-      <sup>
-        {lib.version} - {lib.filename}
-      </sup>
-      <p>{lib.description}</p>
-      <p>{lib.latest}</p>
+      <div className="libs-info">
+        <h1 className="libs-name">{lib.name}</h1>
+        <sup>
+          {lib.version} - {lib.filename}
+        </sup>
+        <p>{lib.description}</p>
+      </div>
+
+      <CopyToClipboard onCopy="" text={lib.latest}>
+        <button data-tip="React-tooltip" data-for="copy-script">
+          Copy
+        </button>
+      </CopyToClipboard>
+
+      <ReactTooltip
+        id="copy-script"
+        place="bottom"
+        type="light"
+        effect="solid"
+        className="tooltip"
+      >
+        <span>Click to copy!</span>
+      </ReactTooltip>
     </div>
   );
 }
